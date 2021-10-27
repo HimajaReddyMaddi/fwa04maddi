c24fbc3cdc3ef3739d6d422c1785b4039699ec52
HimajaReddyMaddi <S545156@nwmissouri.edu>
2021-10-26 20:25:05 -0500

    updated comp.js

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,9 @@ var x=10;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   y= Math.cos(x)
-  res.send(`[Math.cos()] applied to ${x} is  ${y} `);
+  z= Math.asin(x)
+  a=Math.asinh(x)
+  res.send(`[Math.cos()] applied to ${x} is  ${y} \n [Math.asin()] applied to ${x} is  ${z} \n [Math.asinh()] applied to ${x} is  ${a} `);
   ;
  
 });
